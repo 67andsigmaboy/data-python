a4c4ea040fc31faf5db7996d972fd3c38fc73a1e
67andsigmaboy <ahadarablovely@gmail.com>
2026-04-16 22:04:55 +0600

    Update cdhb.py

---
+++ cdhb.py
@@ -0,0 +1,8 @@
+setx={"group1","group2","group3","group4","group5"}
+sety={"group6","group7","group8","group9","group10"}
+print("Original set1:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
